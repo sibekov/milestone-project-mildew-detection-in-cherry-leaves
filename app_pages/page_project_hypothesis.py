da465397fff840a251af248d7d1e9cc315b5d3fd
sibekov <vusimuzij.sibeko@gmail.com>
2024-04-20 17:11:48 +0000

    Add the evaluate function

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -10,4 +10,4 @@ def page_project_hypothesis_body():
         f"typically in the middle of the cell, that can differentiate them from an uninfected cell. \n\n"
         f"* An Image Montage shows that typically a parasitised cell has purplish marks across it. "
         f"Average Image, Variability Image and Difference between Averages studies did not reveal "
-        f"any clear pattern to differentiate one from another."
+        f"any clear pattern to differentiate one from another.")
