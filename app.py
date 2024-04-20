9adc30596a06e5da20feffbee898a74327652fc6
sibekov <vusimuzij.sibeko@gmail.com>
2024-04-20 16:59:26 +0000

    update app.py file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_leaves_visualizer import page_leaves_visualizer_body
-from app_pages.page_owdery_miledew_detector import page_powdery_mildew_detector_body
+from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
 
